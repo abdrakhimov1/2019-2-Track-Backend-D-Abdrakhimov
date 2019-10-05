8146acf8575f2ed45d64af8e3457dbaa5e55b579
abdrakhimov1 <dan.abdrakhimov@yandex.ru>
2019-10-05 23:30:02 +0300

    Added tests.

---
+++ tic-tac-toe/tests.py
@@ -0,0 +1,18 @@
+import unittest
+import tic_tac_toe_user
+import tic_tac_toe
+
+test_form = tic_tac_toe.game_form
+
+#FIXME Что тут тестить?
+
+class Test(unittest.TestCase):
+    
+    # def test_sum(self):
+    #     self.assertEqual(test_form.game_play(), 0, "Should be 0")
+    pass
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
